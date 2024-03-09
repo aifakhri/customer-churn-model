3c56660606129af4f5d9660b745262ae569bd60c
aifakhri <fakhri.azzhar@gmail.com>
2024-03-09 20:03:43 +0700

    Change some variable naming and revise the fold_length

--- ml_from_scratch/_resampling.py
+++ ml_from_scratch/_resampling.py
@@ -10,34 +10,33 @@ class KFold:
         random_state=42
     ):
         self.n_folds = n_folds
-        random_state = random_state
+        self.random_state = random_state
 
 
-    def _iter_folds(self, X):
+    def _iter_indices(self, X):
         """Iterate the data based on the n_folds
         """
 
         n_samples = X.shape[0]
-        indices = np.empty(n_samples)
+        indices = np.arange(n_samples)
 
         np.random.seed(self.random_state)
         np.random.shuffle(indices)
 
 
         # Define fold size and its length
-        fold_length = np.splits // n_samples
-        fold_sizes = np.empty((self.n_fold, fold_length))
+        fold_length = n_samples // self.n_folds 
+        fold_sizes = np.full(self.n_folds, fold_length, dtype="int")
 
         # Fold remainder for uneven splits
         fold_remainders = n_samples % self.n_folds
 
         # Assign the remainder to each folds
         fold_sizes[:fold_remainders] += 1
-
         # Assign data (index) to the fold
         current = 0
         for fold_size in fold_sizes:
-            start, stop = current, current + fold_size
+            start, stop = current, current+fold_size
             yield indices[start:stop]
             current = stop
 
@@ -45,10 +44,10 @@ class KFold:
         """Function to split data based on the n_folds
         """
 
-        n_sample = X.shape[0]
-        indices = np.empty(n_sample)
+        n_samples = X.shape[0]
+        indices = np.arange(n_samples)
 
-        for valid_idx in self._iter_folds(X):
+        for valid_idx in self._iter_indices(X):
             train_index = np.array(
                 [idx for idx in indices if idx not in valid_idx]
             )
